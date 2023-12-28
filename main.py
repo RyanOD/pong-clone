5b006e0121975eef0d86c91f8c72d69b25f720bf
Ryan O'Donnell <odonnell.joseph@gmail.com>
2023-12-27 23:32:35 -0800

    Added collision detection on top and bottom walls.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 @namespace
 class SpriteKind:
     Environment = SpriteKind.create()
+
+def on_a_pressed():
+    ball.set_velocity(-50, 50)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
 def draw_background():
     global background, screen_pos_x, screen_pos_y
     scene.set_background_color(15)
@@ -21,11 +26,19 @@ def create_paddles(width: number, height: number):
         paddle
     """), SpriteKind.player)
     paddle_rt.set_position(scene.screen_width() - 10, scene.screen_height() / 2)
+def ball_collision():
+    if ball.y < 0:
+        ball.y = 2
+        ball.vy = ball.vy * -1
+    elif ball.y > scene.screen_height():
+        ball.y = scene.screen_height() - 2
+        ball.vy = ball.vy * -1
 paddle_rt: Sprite = None
 paddle_lt: Sprite = None
 screen_pos_y = 0
 screen_pos_x = 0
 background: Image = None
+ball: Sprite = None
 game.splash("Welcome to Pong", "Press A to Begin")
 scene.set_background_image(draw_background())
 paddle_width = 4
@@ -37,6 +50,7 @@ ball = sprites.create(assets.image("""
 ball.set_position(scene.screen_width() - 10 - paddle_width * 1.5, 59)
 
 def on_on_update():
+    ball_collision()
     if paddle_lt.y < paddle_height / 2 + 2:
         paddle_lt.y = paddle_height / 2 + 2
     if paddle_lt.y > scene.screen_height() - (paddle_height / 2 + 2):
--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 namespace SpriteKind {
     export const Environment = SpriteKind.create()
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    ball.setVelocity(-50, 50)
+})
 function draw_background () {
     scene.setBackgroundColor(15)
     background = image.create(scene.screenWidth(), scene.screenHeight())
@@ -18,19 +21,32 @@ function create_paddles (width: number, height: number) {
     paddle_rt = sprites.create(assets.image`paddle`, SpriteKind.Player)
     paddle_rt.setPosition(scene.screenWidth() - 10, scene.screenHeight() / 2)
 }
+function ball_collision () {
+    if (ball.y < 0) {
+        ball.y = 2
+        ball.vy = ball.vy * -1
+    } else if (ball.y > scene.screenHeight()) {
+        ball.y = scene.screenHeight() - 2
+        ball.vy = ball.vy * -1
+    }
+}
 let paddle_rt: Sprite = null
 let paddle_lt: Sprite = null
 let screen_pos_y = 0
 let screen_pos_x = 0
 let background: Image = null
+let ball: Sprite = null
 game.splash("Welcome to Pong", "Press A to Begin")
 scene.setBackgroundImage(draw_background())
+info.player1.setScore(0)
+info.player2.setScore(0)
 let paddle_width = 4
 let paddle_height = 16
 create_paddles(paddle_width, paddle_height)
-let ball = sprites.create(assets.image`ball`, SpriteKind.Projectile)
+ball = sprites.create(assets.image`ball`, SpriteKind.Projectile)
 ball.setPosition(scene.screenWidth() - 10 - paddle_width * 1.5, 59)
 game.onUpdate(function () {
+    ball_collision()
     if (paddle_lt.y < paddle_height / 2 + 2) {
         paddle_lt.y = paddle_height / 2 + 2
     }
