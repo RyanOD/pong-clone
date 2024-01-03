ea2d67bd1a96de5dcf9e1a6e1d0f090d5c4a902e
Ryan O'Donnell <odonnell.joseph@gmail.com>
2024-01-02 18:42:54 -0800

    Added randomness to return volley. Added game reset function.

--- main.ts
+++ main.ts
@@ -1,25 +1,14 @@
 namespace SpriteKind {
     export const Environment = SpriteKind.create()
 }
-function paddle_collision () {
-    if (ball.overlapsWith(paddle_lt) || ball.overlapsWith(paddle_rt)) {
-        music.play(music.createSoundEffect(WaveShape.Triangle, 1687, 326, 255, 0, 20, SoundExpressionEffect.Tremolo, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
-        ball.vx = ball.vx * -1.04
-        if (ball.x < scene.screenWidth() / 2) {
-            ball.x = 12
-        } else {
-            ball.x = scene.screenWidth() - 12
-        }
-    }
-}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    ball.setVelocity(100, 50)
+    ball.setVelocity(-100, 50)
 })
 function computer_ai () {
-    if (paddle_rt.y < ball.y - 10) {
-        paddle_rt.y += 1.5
-    } else if (paddle_rt.y > ball.y + 10) {
-        paddle_rt.y += -1.5
+    if (paddle_rt.y + paddle_height / 2 < ball.y - 10) {
+        paddle_rt.y += 2
+    } else if (paddle_rt.y + paddle_height / 2 > ball.y + 10) {
+        paddle_rt.y += -2
     }
 }
 function draw_background () {
@@ -33,13 +22,25 @@ function draw_background () {
     }
     return background
 }
+function ball_paddle_collision () {
+    if (ball.overlapsWith(paddle_lt) || ball.overlapsWith(paddle_rt)) {
+        music.play(music.createSoundEffect(WaveShape.Triangle, 1687, 326, 255, 0, 20, SoundExpressionEffect.Tremolo, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
+        ball.vx = ball.vx * -1.04
+        ball.vy = ball.vy + randint(0, 50)
+        if (ball.x < scene.screenWidth() / 2) {
+            ball.x = 12
+        } else {
+            ball.x = scene.screenWidth() - 12
+        }
+    }
+}
 function create_paddles (width: number, height: number) {
     paddle_lt = sprites.create(assets.image`paddle`, SpriteKind.Player)
     paddle_lt.setPosition(10, scene.screenHeight() / 2)
     paddle_rt = sprites.create(assets.image`paddle`, SpriteKind.Player)
     paddle_rt.setPosition(scene.screenWidth() - 10, scene.screenHeight() / 2)
 }
-function ball_collision () {
+function ball_wall_collision () {
     if (ball.y < 0) {
         ball.y = 2
         ball.vy = ball.vy * -1
@@ -48,25 +49,34 @@ function ball_collision () {
         ball.vy = ball.vy * -1
     }
 }
+function game_reset () {
+    sprites.destroy(ball)
+    ball = sprites.create(assets.image`ball`, SpriteKind.Projectile)
+    ball.setPosition(scene.screenWidth() - 10 - paddle_width * 1.5, 59)
+}
+let paddle_lt: Sprite = null
 let screen_pos_y = 0
 let screen_pos_x = 0
 let background: Image = null
 let paddle_rt: Sprite = null
-let paddle_lt: Sprite = null
 let ball: Sprite = null
+let paddle_height = 0
+let paddle_width = 0
 game.splash("Welcome to Pong", "Press A to Begin")
 scene.setBackgroundImage(draw_background())
 info.player1.setScore(0)
 info.player2.setScore(0)
-let paddle_width = 4
-let paddle_height = 16
+paddle_width = 4
+paddle_height = 16
 create_paddles(paddle_width, paddle_height)
-ball = sprites.create(assets.image`ball`, SpriteKind.Projectile)
-ball.setPosition(scene.screenWidth() - 10 - paddle_width * 1.5, 59)
+game_reset()
 game.onUpdate(function () {
+    if (ball.x > scene.screenWidth() || ball.x < 0) {
+        game_reset()
+    }
     computer_ai()
-    ball_collision()
-    paddle_collision()
+    ball_wall_collision()
+    ball_paddle_collision()
     if (paddle_lt.y < paddle_height / 2 + 2) {
         paddle_lt.y = paddle_height / 2 + 2
     }
